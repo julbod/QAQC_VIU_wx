78f7745b01fb8593fbd02bf2272d3723b5c474b0
Julien Bodart <43612194+julbod@users.noreply.github.com>
2024-07-24 13:53:05 +0200

    modified the way to find indices with np.where

--- qaqc_functions.py
+++ qaqc_functions.py
@@ -280,7 +280,7 @@ def SWE_summer_zeroing(data_all, data_subset, flag, dt_yr, dt_summer_yr, summer_
     # detection does not work properly), then run below
     name = pd.concat([pd.DataFrame([wx_stations_name],columns=['filename']), pd.DataFrame([year],columns=['zero_dates'])], axis=1, join='inner')
     if np.any((df_csv.values == name.values).all(axis=1)) == True:
-        idx = np.where(df_csv.values == name.values)[0][0]
+        idx = int(np.flatnonzero((df_csv.values == name.values).all(axis=1))[0]) if np.any((df_csv.values == name.values).all(axis=1)) else None
         idx_longest_sequence = np.where(csv_dt[idx] == dt)[0][0]
 
     # else if there is no specific dates in the csv (i.e. where the below code
