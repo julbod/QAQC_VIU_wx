68379d73557ef103a35824d1fff81bceb866eb1c
Julien Bodart <43612194+julbod@users.noreply.github.com>
2024-07-25 15:00:36 +0200

    pushing qaqc weekly for all stations and years

--- qaqc_stations_list.py
+++ qaqc_stations_list.py
@@ -10,27 +10,27 @@
 
 #%% To qaqc (22 stations in total, as of 19.07.2024)
 clean_apelake = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_cainridgerun = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth']
-# clean_claytonfalls = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_datlamen = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed']
-# clean_eastbuxton = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Snow_Depth']
-# clean_homathko = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_klinaklini = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_lowercain = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Snow_Depth', 'SWE']
-# clean_machmellkliniklini = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_mountarrowsmith = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_mountcayley = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth']
-# clean_mountmaya = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
-# clean_perseverance = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_placeglacier = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth'] # 'Snow_Depth' is not yet calibrated
-# clean_plummerhut = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir'] # 'Snow_Depth removed due to impossible automated qaqc process there
-# clean_rennellpass = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed'] # 'Snow_Depth removed due to impossible automated qaqc process there
-# clean_steph3 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
-# clean_steph6 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
-# clean_tetrahedron = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_uppercruickshank = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
-# clean_upperrussell = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe']
-# clean_upperskeena = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_cainridgerun = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth']
+clean_claytonfalls = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_datlamen = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed']
+clean_eastbuxton = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Snow_Depth']
+clean_homathko = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_klinaklini = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_lowercain = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Snow_Depth', 'SWE']
+clean_machmellkliniklini = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_mountarrowsmith = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_mountcayley = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth']
+clean_mountmaya = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
+clean_perseverance = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_placeglacier = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth'] # 'Snow_Depth' is not yet calibrated
+clean_plummerhut = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir'] # 'Snow_Depth removed due to impossible automated qaqc process there
+clean_rennellpass = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed'] # 'Snow_Depth removed due to impossible automated qaqc process there
+clean_steph3 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
+clean_steph6 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
+clean_tetrahedron = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_uppercruickshank = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_upperrussell = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe']
+clean_upperskeena = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 
 #%% not yet in the qaqc pipeline (Stephs are not connected to live transmission
 # and Machmell station is down since 2023)
