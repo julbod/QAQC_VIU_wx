53cb2d1556f8c236a3647285877d9c586cada8bb
Julien Bodart <43612194+julbod@users.noreply.github.com>
2024-07-25 14:16:28 +0200

    Added place glacier and put machmell in the non-qaqc list until it's back live

--- qaqc_stations_list.py
+++ qaqc_stations_list.py
@@ -17,12 +17,12 @@ clean_eastbuxton = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pi
 clean_homathko = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 clean_klinaklini = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 clean_lowercain = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Snow_Depth', 'SWE']
-clean_machmell = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed']
 clean_machmellkliniklini = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 clean_mountarrowsmith = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 clean_mountcayley = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth']
 clean_mountmaya = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 clean_perseverance = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+clean_placeglacier = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth'] # 'Snow_Depth' is not yet calibrated
 clean_plummerhut = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir'] # 'Snow_Depth removed due to impossible automated qaqc process there
 clean_rennellpass = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed'] # 'Snow_Depth removed due to impossible automated qaqc process there
 clean_steph3 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
@@ -33,14 +33,14 @@ clean_upperrussell = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe']
 clean_upperskeena = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
 
 #%% not yet in the qaqc pipeline (Stephs are not connected to live transmission
-# and place glacier has just been installed and is still in its trial phase)
+# and Machmell station is down since 2023)
 # clean_steph1 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 # clean_steph2 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 # clean_steph4 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 # clean_steph7 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 # clean_steph8 = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Snow_Depth']
 # clean_russellmain = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Pk_Wind_Speed']
-# clean_placeglacier = ['Air_Temp', 'RH', 'BP', 'PP_Tipper', 'PC_Tipper', 'PC_Raw_Pipe', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed', 'Pk_Wind_Dir', 'Snow_Depth', 'SWE']
+# clean_machmell = ['Air_Temp', 'RH', 'PP_Tipper', 'PC_Tipper', 'Wind_Speed', 'Wind_Dir', 'Pk_Wind_Speed']
 
 #%%
 
