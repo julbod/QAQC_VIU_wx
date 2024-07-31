0aac2ab51eb690d956eb8fe7bb21319006b50c69
Julien Bodart <43612194+julbod@users.noreply.github.com>
2024-07-31 14:08:40 +0200

    Updated pull of dbs based on stations_list file instead on many lines of codes which include/exclude specific stations. Both work, but the latter is more efficient. Also added a pool cycling time of 3600 to avoid error with connection closed by peers

--- fill_db_nans_weekly.py
+++ fill_db_nans_weekly.py
@@ -44,54 +44,17 @@
 import pandas as pd 
 from sqlalchemy import create_engine
 import numpy as np
-import re
 from datetime import datetime
 import datetime as dtime
 
 #%% import support functions
-import qaqc_functions
+from qaqc_stations_list import *
 
 #%% Establish a connection with MySQL database 'viuhydro_wx_data_v2'
-engine = create_engine('mysql+mysqlconnector://viuhydro_shiny:.rt_BKD_SB*Q@192.99.62.147:3306/viuhydro_wx_data_v2', echo = False)
+engine = create_engine('mysql+mysqlconnector://viuhydro_shiny:.rt_BKD_SB*Q@192.99.62.147:3306/viuhydro_wx_data_v2', echo = False, pool_pre_ping=True, pool_recycle=3600)
 
-#%% extract name of all tables within SQL database and clean up var name list
-connection = engine.raw_connection()
-cursor = connection.cursor()
-cursor.execute("Show tables;")
-wx_stations_lst = cursor.fetchall()
-wx_stations = []
-for i in range(len(wx_stations_lst)):
-     lst = (re.sub(r'[^\w\s]', '', str(wx_stations_lst[i])))
-     wx_stations.append(lst)
-   
-# remove 'raw' tables, remove all steph (but steph3), and others due to local issues
-# or because there is no snowDepth sensor there, and sort out the name formatting
-wx_stations = [x for x in wx_stations if "clean" in x ]
-wx_stations = [x for x in wx_stations if not "legacy" in x] # remove legacy data for Cairnridgerun
-wx_stations = [x for x in wx_stations if not "test" in x] # remove test databases
-wx_stations = [x for x in wx_stations if not "archive" in x] # remove archive from list
-wx_stations = [x for x in wx_stations if not "backup" in x] # remove archive from list
-
-# deal with Stephanies that are or are not connected to satellite live transmission
-keep_steph = False
-
-# if you don't want Stephanies that are not connected to satellite, then remove
-#  all except from Steph 3, 6 and Upper Russell
-if keep_steph == False:
-    wx_stations = [w.replace('clean_steph3', 'clean_Stephanie3') for w in wx_stations] # rename steph3 so it doesn't get cut out
-    wx_stations = [w.replace('clean_steph6', 'clean_Stephanie6') for w in wx_stations] # rename steph6 so it doesn't get cut out
-    wx_stations = [w.replace('clean_upperrussell', 'clean_Upper') for w in wx_stations] # rename upper russell so it doesn't get cut out
-    wx_stations = [x for x in wx_stations if not "steph" in x] # remove all stephanies
-    wx_stations = [w.replace('clean_Upper', 'clean_upperrussell') for w in wx_stations] # rename machmellkliniklini back to original
-    wx_stations = [w.replace('clean_Stephanie3', 'clean_steph3') for w in wx_stations] # rename steph3 back to original
-    wx_stations = [w.replace('clean_Stephanie6', 'clean_steph6') for w in wx_stations] # rename steph6 back to original
-    wx_stations = [w.replace('clean_Upper', 'clean_upperrussell') for w in wx_stations] # rename machmellkliniklini back to original
-    stephs = []
-    
-# else if you want Stephanies not connected to satellite, select specific ones 
-else:
-    stephs = ['steph1', 'steph2', 'steph4', 'steph7', 'steph8', 'russellmain'] # string with all Stephanies except Steph3, 6 and upper russell
-
+#%% create list of stations to qaqc for this variable
+wx_stations = {name: globals()[name] for name in globals() if name.startswith('clean_')}
 wx_stations_name = list(map(lambda st: str.replace(st, 'clean_', ''), wx_stations)) # remove 'clean_' for csv export
 wx_stations_name_cap = [wx_name.capitalize() for wx_name in wx_stations_name] # capitalise station name
 
@@ -110,32 +73,18 @@ for l in range(len(wx_stations_name)):
 
     #%% Only select earliest possible date for full year
     dt_sql = pd.to_datetime(sql_file['DateTime'])    
+    qaqc_upToDate = (datetime.now()- dtime.timedelta(days=7)).strftime("%Y-%m-%d %H") + ':00:00' # todays date rounded to nearest hour
     
-    #%% only keep data from oldest to newest default date except for exceptions  
-    # Stephs not connected to satellite have data up to Oct 2023
-    if wx_stations_name[l] in stephs: 
-        end_yr_sql = qaqc_functions.nearest(dt_sql, datetime(2023, 9, 30, 23, 00, 00))
-        new_df = sql_file.loc[:np.where(sql_file['DateTime'] == end_yr_sql)[0][0]]
-     
-    # Machmell went offline in Feb 2023
-    elif wx_stations_name[l] == 'machmell':
-        end_yr_sql = qaqc_functions.nearest(dt_sql, datetime(2023, 2, 12, 11, 00, 00))
-        new_df = sql_file.loc[:np.where(sql_file['DateTime'] == end_yr_sql)[0][0]]
-        
-    # otherwise if any other stations, then select last week as latest date
-    else:
-        qaqc_upToDate = (datetime.now()- dtime.timedelta(days=7)).strftime("%Y-%m-%d %H") + ':00:00' # todays date rounded to nearest hour
-        
-        # try unless it throws error (which happens only if the transmission 
-        # has stopped between the last time this code ran and the qaqc_upToDate
-        # date (i.e. over the last week))
-        try:
-            new_df = sql_file[:sql_file.loc[sql_file['DateTime'] == qaqc_upToDate].iloc[0].name] # today's date - 7 days
-        except IndexError:
-            # if transmission has stopped since last week, skip this station
-            print('Careful: %s has stopped transmitting and will not be qaqced until back on live' %(sql_name))     
-            continue
-    
+    # try unless it throws error (which happens only if the transmission 
+    # has stopped between the last time this code ran and the qaqc_upToDate
+    # date (i.e. over the last week))
+    try:
+        new_df = sql_file[:sql_file.loc[sql_file['DateTime'] == qaqc_upToDate].iloc[0].name] # today's date - 7 days
+    except IndexError:
+        # if transmission has stopped since last week, skip this station
+        print('Careful: %s has stopped transmitting and will not be qaqced until back on live' %(sql_name))     
+        continue
+
     # add nans if missing records        
     nanout = [c for c in new_df.columns if c not in ['DateTime', 'WatYr']]
     new_df.loc[:, nanout] = np.nan
